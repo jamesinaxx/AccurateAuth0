d6437084728515eb5c6402410a84c429f7fc087d
jamesinaxx <grassojames5@gmail.com>
2021-06-15 09:26:35 +1000

    Downgraded some js

--- authTitle.js
+++ authTitle.js
@@ -1,12 +1,14 @@
 let old0text = 'everyone';
 let old1text = 'just anyone';
 
-window.setInterval(() => {
-	let items = document.getElementsByClassName('slide-enter-done');
-	let text = document.querySelector('title').innerText;
-	text = text.replace(old0text, items[0].innerText);
-	text = text.replace(old1text, items[1].innerText);
-	document.querySelector('title').innerText = text;
-	old0text = items[0].innerText;
-	old1text = items[1].innerText;
-}, 1000);
+window.addEventListener('load', function () {
+	window.setInterval(function () {
+		let items = document.getElementsByClassName('slide-enter-done');
+		let text = document.querySelector('title').innerText;
+		text = text.replace(old0text, items[0].innerText);
+		text = text.replace(old1text, items[1].innerText);
+		document.querySelector('title').innerText = text;
+		old0text = items[0].innerText;
+		old1text = items[1].innerText;
+	}, 1000);
+});
